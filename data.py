a2e9daf99ca5a9ab675ca8f5337070e3c9fda531
olivier-data <73345139+olivier-data@users.noreply.github.com>
2020-12-01 16:51:21 +0100

    Create data.py

--- data.py
+++ data.py
@@ -27,7 +27,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 get_ipython().run_line_magic('matplotlib', 'inline')
-
+from sklearn.preprocessing import MinMaxScaler
 print("hello this for sharing group code")
 import yfinance as yf
 
