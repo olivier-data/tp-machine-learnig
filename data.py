966a9e94a6f9ddb664e5962b07fb8b75c7886aaa
olivier-data <73345139+olivier-data@users.noreply.github.com>
2020-12-01 14:50:40 +0100

    Create data.py

--- data.py
+++ data.py
@@ -1,5 +1,33 @@
-# Created by Luisa Hernandez Z.
+
+
+#1 La récupération de données :  cotation Airbus depuis 2010
+#2 Le traitement et l’analyse : 
+#2.1 Éliminer les NaN et les données non pertinentes
+#2.2 traiter
+#3 La représentation / application : Établir des graphique
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#Created on 01 12 2020
+########
+
+#@author: Luisa,Ismail,Mamy, Olivier
+
 #https://github.com/Benny-/Yahoo-ticker-symbol-downloader
+
+#1 La récupération de données : cotation Airbus depuis 2010
+
+#!/usr/bin/env python
+# coding: utf-8
+# TP ml Data scientist
+
+# importing necessary libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+get_ipython().run_line_magic('matplotlib', 'inline')
+
 print("hello this for sharing group code")
 import yfinance as yf
 
