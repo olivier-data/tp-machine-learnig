bd604e76719f937e0ef894fbf673b0148b51812d
luisaher-lab <luisahernandezzabala@gmail.com>
2020-12-01 11:34:47 +0100

    hello group this is for sharing our code

--- data.py
+++ data.py
@@ -1,3 +1,10 @@
 # Created by Luisa Hernandez Z.
 
-print("hello")
+print("hello this for sharing group code")
+import yfinance as yf
+
+airbus ='AIR.PA'
+data = yf.Ticker(airbus)
+
+dataDF = data.history(period='1d',satart = '2000-12-01',end='2020-11-30')
+
