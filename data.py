6c1228c6953091cc128b9132361fbb6fd69ee811
luisaher-lab <luisahernandezzabala@gmail.com>
2020-12-01 11:59:22 +0100

    hello group this is for sharing our code

--- data.py
+++ data.py
@@ -1,5 +1,5 @@
 # Created by Luisa Hernandez Z.
-
+#https://github.com/Benny-/Yahoo-ticker-symbol-downloader
 print("hello this for sharing group code")
 import yfinance as yf
 
@@ -8,3 +8,8 @@ data = yf.Ticker(airbus)
 
 dataDF = data.history(period='1d',start = '2000-12-01',end='2020-11-30')
 
+
+dataDF.to_csv(r'C:\Users\Luisa.HERNANDEZ-ZABA\PycharmProjects\tp-machine-learnig\data_airbus.csv', index = False)
+
+
+print("end of code")
